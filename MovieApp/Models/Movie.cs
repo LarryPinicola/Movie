551c3c5dc0c7876bcc2251728b5b70f5676ae5a5
LarryPinicola <thelarry1549@gmail.com>
2023-12-11 11:03:25 +0630

    Add New MovieApp [InProgress]

---
+++ MovieApp/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)] public DateTime ReleaseDate { get; set; }
+
+        public string? Genre { get; set; }
+
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage ="Please choose Movie image")]
+        [Display(Name ="Movie Img")]
+        [NotMapped]
+        public IFormFile MovieImg { get; set; }
+    }
+}
